4ee23a04e146c796c3c0e5e87e731a0809ff3029
Debankan Roy <userx4006@gmail.com>
2026-09-08 17:56:25 +0530

    feat: added spring app

---
+++ inventory-app/src/main/java/dev/zen/inventory/InventoryApplication.java
@@ -0,0 +1,16 @@
+package dev.zen.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "dev.zen.inventory")
+@EnableJpaRepositories(basePackages = "dev.zen.inventory.persistence.repository")
+@EntityScan(basePackages = "dev.zen.inventory.domain.models")
+public class InventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
+}
